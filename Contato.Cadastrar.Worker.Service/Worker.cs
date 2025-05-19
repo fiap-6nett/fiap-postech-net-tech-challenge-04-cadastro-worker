b1aa2d6f1f781595663d43b9817cdfdcf7d7f60e
Wesley Leite <leitewesleyleite@gmail.com>
2025-05-19 18:31:26 -0300

    Ajuste no worker

--- Contato.Cadastrar.Worker.Service/Worker.cs
+++ Contato.Cadastrar.Worker.Service/Worker.cs
@@ -1,51 +1,29 @@
 using Contato.Cadastrar.Worker.Infra.Mensageria.Consumer;
 
-namespace Contato.Cadastrar.Worker.Service;
-
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
     private readonly IContatoConsumer _consumer;
-    private readonly IServiceProvider _serviceProvider;
 
-
-    public Worker(ILogger<Worker> logger, IContatoConsumer consumer, IServiceProvider serviceProvider)
+    public Worker(ILogger<Worker> logger, IContatoConsumer consumer)
     {
         _logger = logger;
         _consumer = consumer;
-        _serviceProvider = serviceProvider;
     }
-
-
-    //protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    //{
-    //    using var scope = _serviceProvider.CreateScope();
-    //    var consumer = scope.ServiceProvider.GetRequiredService<IContatoConsumer>();
-
-    //    while (!stoppingToken.IsCancellationRequested)
-    //    {
-    //        if (_logger.IsEnabled(LogLevel.Information))
-    //        {
-    //            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-    //        }
-
-    //        _consumer.StartConsuming(stoppingToken);
-
-    //        await Task.Delay(1000, stoppingToken);
-    //    }
-    //}
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        if (_logger.IsEnabled(LogLevel.Information))
-        {
-            _logger.LogInformation("Iniciando worker e consumidor da fila...");
-        }
+        _logger.LogInformation("Iniciando worker e consumidor da fila...");
 
         _consumer.StartConsuming(stoppingToken);
 
-        // Mantém o serviço vivo enquanto não for cancelado
         await Task.Delay(Timeout.Infinite, stoppingToken);
     }
 
+    public override void Dispose()
+    {
+        _logger.LogInformation("Encerrando worker e fechando consumidor da fila.");
+        (_consumer as IDisposable)?.Dispose();
+        base.Dispose();
+    }
 }
